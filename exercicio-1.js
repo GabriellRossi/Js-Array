9636f45732a04cafa9e502ac18219f8559b63de0
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-22 03:31:20 -0300

    Praticando utizando o push e pop

---
+++ exercicio-1.js
@@ -0,0 +1,16 @@
+// const arrayVazia = [1, 2, 3]
+// console.log(arrayVazia[3]);
+// console.log(arrayVazia.length);
+
+
+jantarDeHoje = ['🍔', '🌭', '🍟']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+
+jantarDeHoje.push('🥣')
+jantarDeHoje.push('🥐')
+jantarDeHoje.push('🧁')
+
+console.log(jantarDeHoje);
