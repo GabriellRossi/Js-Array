34c54c970a0c3ee7672fc21033c4cea8e74ab1d0
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-22 20:32:43 -0300

    Alterando um elemento do array com o splice

---
+++ atualizando-lista.js
@@ -0,0 +1,7 @@
+const listaDeChamadas = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
+
+// listaDeChamadas.splice(1, 2, 'Rodrigo')
+
+listaDeChamadas.splice(2,0, 'Rodrigo')
+
+console.log(`A lista de chamada: ${listaDeChamadas}`)
