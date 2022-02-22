4fceb907a842a32e349bcd1961ba095d390b16f1
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-22 20:47:09 -0300

    Utizando o Concat para concatenar os elementos em só array

---
+++ juntando-turma.js
@@ -0,0 +1,10 @@
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo']
+
+const salaDeJavaScript = ['Ju', 'Leo', 'Raquel']
+
+const salaUnificadas = salaDePython.concat(salaDeJavaScript)
+
+console.log(salaUnificadas)
+
+
+// Concat é: Juntar os array, Concatenar.
