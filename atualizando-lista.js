d184fd58660bf2842b5c57cec5c89077e5f2a77e
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-22 20:35:06 -0300

    Utilizando Slice que é para adicionar ou remover um elemento de uma lista

--- atualizando-lista.js
+++ atualizando-lista.js
@@ -5,3 +5,6 @@ const listaDeChamadas = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo']
 listaDeChamadas.splice(2,0, 'Rodrigo')
 
 console.log(`A lista de chamada: ${listaDeChamadas}`)
+
+
+// O splice é usado para adicionar e remover elementos de uma lista.
