60fe8ac1706abb5c893cd14e3ecdd453bb19311e
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-23 03:23:28 -0300

    Localizando um aluno com duas dimensões de array com includes e indexof

---
+++ localizando-aluno.js
@@ -0,0 +1,19 @@
+const alunos = ['Joao', 'Juliana', 'Caio', 'Ana']
+
+const mediasDosAlunos = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
+
+const exibeNomeNota = (nomeDoAluno) => {
+  if(listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
+    let indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno)
+    return listaDeNotasEAlunos[0][indice] + ',sua média é ' + listaDeNotasEAlunos[1][indice]
+  } else {
+    return `Aluno não cadastrado`
+  }
+}
+
+console.log(exibeNomeNota("Juliana"))
+
+// Includes -> Retorna um Booleano, True ou False
+// indexOf -> 3
