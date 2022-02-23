c37f8484f79eea4eadc21bc260f227978cf00458
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-23 14:03:18 -0300

    Utilizando o foreach criando função e usando com parâmetros

---
+++ callbacks.js
@@ -0,0 +1,9 @@
+const nomes = ['Ana', 'Pedro', 'Ju', 'Leonardo']
+
+nomes.forEach(imprimeNomes)
+
+function imprimeNomes(nome) {
+  console.log(nome)
+}
+
+// Utilizando Callback com uma outra forma criando com funções e passando o parâmetros.
