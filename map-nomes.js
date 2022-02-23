1c910980b6726d136740a464fc4d79415e0e90c2
GabriellRossi <gabriel.rossi159@gmail.com>
2022-02-23 14:24:11 -0300

    Utilizando um metodo do map toupperCase, que percorre o array e transforma todos elementos em letras maiscúlas

--- map-nomes.js
+++ map-nomes.js
@@ -3,3 +3,5 @@ let nomes = ['ana Julia', 'Caio Vinicius', 'BIA silva']
 const nomesAtualizados = nomes.map(nome => nome.toUpperCase())
 
 console.log(nomesAtualizados)
+
+// Map -> Utilizando um metodo do map toupperCase, que percorre o array e transforma todos elementos em letras maiscúlas
